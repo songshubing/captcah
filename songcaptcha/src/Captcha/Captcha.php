9c3f83fa0f8b4f2d35ec3aecf05f325d6e92c7d5
songshubing <468718879@qq.com>
2018-03-13 15:10:51 +0800

    修改

--- songcaptcha/src/Captcha/Captcha.php
+++ songcaptcha/src/Captcha/Captcha.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Song\Captcha;
+namespace Captcha;
 
 class Captcha{
     /**
--- songcaptcha/src/Captcha/Image.php
+++ songcaptcha/src/Captcha/Image.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Song\Captcha;
+namespace Captcha;
 
 use Symfony\Component\HttpFoundation\Response;
 
